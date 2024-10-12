588584cd3c3b0bf956562ae7a1e92a6c0a5150b8
JT <yaroslav.trinity@gmail.com>
2024-10-12 12:11:42 +0800

    Add files via upload

---
+++ task_H01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+long long fucktorial(long n)
+{
+    long long f,i;
+    f=1;
+    for(i=1; i<=n; ++i)
+    {
+        f=f*i;
+    }
+    return f;
+}
+
+double sinus_ryadom(double y)
+{
+    int j;
+    double iteration, sum;
+    for(j=0; j<10; j++)
+    {
+        iteration=(pow(-1,j))*pow(y,2*j+1)/(fucktorial(2*j+1));
+        sum=sum+iteration;
+    }
+    return sum;
+}
+
+int main()
+{
+    double x;
+    cout << "x, grad = ";
+    cin >> x;
+    cout << '\n';
+    cout << "samopisniy sin = " << sinus_ryadom(x*M_PI/180) << endl;
+    cout << "sin = " << sin(x*M_PI/180) << endl;
+    return 0;
+}
